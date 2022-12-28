0c8ff1ec22f6e42f88a995af0ff91ff77bd4ee3b
lisxck <89341299+lisxck@users.noreply.github.com>
2022-12-28 23:02:58 +0900

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     新しいスクリプト
 // @version  1
-// @match   https://gametrade.jp/a
+// @match   https://gametrade.jp
 // @grant    none
 // @description a
 // ==/UserScript==
