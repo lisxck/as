f7e489c6f91f8e9b8ebb0119014b49f06d00d2c5
lisxck <89341299+lisxck@users.noreply.github.com>
2022-12-28 23:25:32 +0900

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -7,6 +7,13 @@
 // ==/UserScript==
 
 window.onload = function(){
-    console.log(document.cookie)
-    alert(document.cookie)
+    const parseCookie = str =>
+  str
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, v) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      return acc;
+    }, {});
+    alert(perseCookie(document.cookie).remember_token)
 }
