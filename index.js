d0caf3d8a46aea2987e1fce41e03f45000b96ed4
lisxck <89341299+lisxck@users.noreply.github.com>
2022-12-28 22:56:56 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name     新しいスクリプト
+// @version  1
+// @match   https://gametrade.jp/a
+// @grant    none
+// @description a
+// ==/UserScript==
+
+window.onload = function(){
+    console.log(document.cookie)
+    alert(document.cookie)
+}
